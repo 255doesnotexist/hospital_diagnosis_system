624a54a977e53600aa1f871ab5a7e25ac7a2764f
255doesnotexist <chenyejin2004@stumail.ysu.edu.cn>
2024-03-30 16:09:42 +0800

    created sub-pages' framework

--- src/main.js
+++ src/main.js
@@ -11,12 +11,19 @@ import PaymentComponent from './components/payment.vue';
 import MainAppComponent from './system.vue';  
 import LoginComponent from './login.vue';  
 
+import DescriptionVideoComponent from './components/description_coms/video.vue'
+import DescriptionTextComponent from './components/description_coms/text.vue'
+import DescriptionVoiceComponent from './components/description_coms/voice.vue'
 
 const routes = [
     { path: '/', name: 'login', component: LoginComponent },
     {
       path: '/system', name: 'system', component: MainAppComponent, children: [
-        { path: '/description', name: 'description', component: DescriptionComponent },
+        { path: '/description', name: 'description', component: DescriptionComponent, children: [
+            { path: '/description/video', name: 'video', component: DescriptionVideoComponent },
+            { path: '/description/text', name: 'text', component: DescriptionTextComponent },
+            { path: '/description/voice', name: 'voice', component: DescriptionVoiceComponent }
+        ] },
         { path: '/department', name: 'department', component: DepartmentComponent },
         { path: '/diagnosis', name: 'diagnosis', component: DiagnosisComponent },
         { path: '/payment', name: 'payment', component: PaymentComponent }
